684f8247f7dfc79896fb15f19a2448d3b9660851
Victor Macêdo <joaovictormacedo.santos99@gmail.com>
2023-11-06 19:46:05 -0300

    Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Initial commit
+    <main className="flex min-h-screen flex-col justify-between bg-dark-10">
+      <header className="flex min-w-full h-20 justify-center">
+        <div className="flex w-[1080px] h-full justify-between">
+          <div>
+            <h1 className="flex h-full items-center text-brand-color">
+              Victor Macêdo
+            </h1>
+          </div>
+          <nav>
+            <ul className="flex h-full gap-x-10 items-center justify-between text-dark-30">
+              <li>Home</li>
+              <li>Sobre mim</li>
+              <li>Experiência</li>
+              <li>Projetos</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,17 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'brand-color': '#00D2DF',
+      'dark-10': '#121212',
+      'dark-20': '#151515',
+      'dark-30': '#C4C4C4',
+      'support-01': '#2962FF',
+      'support-02': '#0BB07B',
+      'support-03': '#FFCE52',
+      'support-04': '#F03D3D',
+      'support-05': '#E0E0E0'
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
